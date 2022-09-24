e7187171010cf27ceddb070237f5adbbfbd0840f
Feline <zxf_tech@outlook.com>
2022-09-24 16:40:01 +0800

    feat:添加 cross-env

--- app.ts
+++ app.ts
@@ -1,11 +1,15 @@
 import Koa from "koa";
+import http from "http";
 
 const app = new Koa();
+
+const server: http.Server = new http.Server(app.callback());
 
 app.use(async (ctx) => {
   ctx.body = "Hello Koa";
 });
 
 app.listen(3000, () => {
-  console.log("server start!");
+  console.log("server start at prot 3000");
+  console.log(process.env.NODE_ENV);
 });
