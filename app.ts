a6ff8cb108abc1e1cfaf98d5d2b5c46e6ce54b90
Feline <zxf_tech@outlook.com>
2022-10-03 22:34:04 +0800

    feat:app全局配置Config相关

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@ import http from "http";
 
 import initCore from "./src/core/Init";
 
+import Config from "./src/config/config";
+
 const app = new Koa();
 
 const server: http.Server = new http.Server(app.callback());
@@ -10,7 +12,7 @@ const server: http.Server = new http.Server(app.callback());
 // 中间件 初始化
 initCore(app, server);
 
-app.listen(3000, () => {
-  console.log("server start at prot 3000");
-  console.log(process.env.NODE_ENV);
+app.listen(Config.HTTP_PORT, () => {
+  console.log(`Server start at port ${Config.HTTP_PORT}`);
+  console.log(`Operating on ${process.env.NODE_ENV}`);
 });
---
+++ src/config/config.ts
@@ -0,0 +1,27 @@
+const isDev = process.env.NODE_ENV === "develop";
+
+export default class Config {
+  // 环境
+  public static readonly ENV = isDev;
+  // 服务器端口
+  public static readonly HTTP_PORT = 9000;
+  // 接口前缀
+  public static readonly API_PREFIX = "/api/";
+  // 根目录
+  public static readonly BASE = isDev ? "src" : "dist/src";
+
+  // 默认时间格式
+  public static readonly DEFAULT_DATE_FORMAT = "YYYY-MM-DD HH:mm:ss";
+
+  public static readonly MYSQL = {
+    DB_NAME: "blog",
+    HOST: "127.0.0.1",
+    PORT: 3306,
+    USER_NAME: "root",
+    PASSWORD: "hg178ar6",
+    CONNECTION_LIMIT: 60 * 60 * 1000,
+    CONNECT_TIMEOUT: 1000 * 60 * 60 * 1000,
+    ACQUIRE_TIMEOUT: 60 * 60 * 1000,
+    TIMEOUT: 1000 * 60 * 60 * 1000,
+  };
+}
