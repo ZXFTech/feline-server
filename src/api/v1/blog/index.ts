7ded9aec0b8da2b0d7416fe57ecda10b32efa3f0
Feline <zxf_tech@outlook.com>
2023-02-24 20:54:15 +0800

    blog 相关路由

--- src/api/v1/blog/index.ts
+++ src/api/v1/blog/index.ts
@@ -1,11 +1,17 @@
 import Router from "koa-router";
 
+import blogList from "./blog.json";
+
 const router = new Router({
   prefix: "/blog",
 });
 
 router.get("/list", async (ctx) => {
-  ctx.body = "blog list";
+  ctx.body = blogList;
+});
+
+router.get("/detail/:id", async (ctx) => {
+  ctx.body = blogList[0];
 });
 
 export default router;
