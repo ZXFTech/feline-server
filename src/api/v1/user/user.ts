21d35d0229994ad2e0780eb6842315d927eaecaf
Feline <zxf_tech@outlook.com>
2023-05-06 22:15:43 +0800

    无用代码删除

--- src/api/v1/user/user.ts
+++
@@ -1,8 +0,0 @@
-import Router from "koa-router";
-
-const router = new Router({
-  prefix: "/user",
-});
-
-
-router.get('/user/info')
--- src/server/mysql/pool.ts
+++
@@ -1,6 +0,0 @@
-import mysql from "mysql2";
-import { mysqlConfig } from "../../config/config";
-
-const pool = mysql.createPool(mysqlConfig);
-
-export default pool;
